c81ff2d1f08ed641fc6d69b1cff5dc2f90e6d627
fnkbsk <igoico10@gmail.com>
2023-08-31 20:41:02 -0300

    metodosget

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,10 +3,24 @@ import productModel from "../models/products.models.js";
 
 const productRouter = Router()
 
+
+//METODO GET
 productRouter.get('/', async (req, res) => {
+    const { limit } = req.query
     try {
-
+        const prods = await productModel.find().limit(limit)
+        res.status(200).send({resultado: 'OK', products: prods})
     } catch (error) {
+        res.status(400).send({error: `Error al consultar productos: ${error}`})
+    }
+});
 
+productRouter.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const prod = await productModel.findById(id)
+        res.status(200).send({resultado: 'OK', product: prod})
+    } catch (error) {
+        res.status(400).send({error: `Error al consultar producto: ${error}`})
     }
 })
