a54b80bc45853d5bcaaa7d0f6b536c43838aad7e
fnkbsk <igoico10@gmail.com>
2023-10-10 18:09:40 -0300

    jwt

---
+++ src/utils/jws.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+    const token = jwt.sign({ user }, "coderhouse123", { expiresIn: '12h'})
+    return token
+}
