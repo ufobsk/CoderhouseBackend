83580de8abe18548a09c147b6f42eb4be0f1fb03
fnkbsk <igoico10@gmail.com>
2023-10-10 18:38:44 -0300

    commit

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,9 +1,28 @@
-import 'dotenv/config';
-import jwt from 'jsonwebtoken';
+import "dotenv/config";
+import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-    const token = jwt.sign({ user }, "coderhouse123", { expiresIn: '12h'})
-    return token
-}
+  const token = jwt.sign({ user }, "coderhouse123", { expiresIn: "12h" });
+  return token;
+};
 
-console.log(generateToken())
+
+console.log(generateToken());
+
+export const authToken = (req, res, next) => {
+  const authHeader = req.headers.Authorization;
+  if (!authHeader) {
+    return res.status(401).send({ error: "Usuario no autenticado" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.sign(token, process.env.JWT_SECRET, (error, credentials) => {
+    if (error) {
+      return res.status(403).send({ error: "Usuario no autorizado" });
+    }
+
+    req.user = credentials.user;
+    next();
+  });
+};
