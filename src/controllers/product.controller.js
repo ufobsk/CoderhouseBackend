893aa0b41ad37bf0031cdeccb49caf5b8cfed7fb
fnkbsk <igoico10@gmail.com>
2023-10-19 19:57:23 -0300

    controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -57,3 +57,37 @@ export const postProduct = async (req, res) => {
         }
     }
 }
+
+export const putProduct = async (req, res) => {
+    const { id } = req.params
+    const { title, description, code, price, stock, category } = req.body
+
+    try {
+        const product = await productModel.findByIdAndUpdate(id, { title, description, code, price, stock, category })
+
+        if (product) {
+            return res.status(200).send(product)
+        }
+
+        res.status(404).send({ error: "Producto no encontrado" })
+
+    } catch (error) {
+        res.status(500).send({ error: `Error en actualizar producto ${error}` })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const product = await productModel.findByIdAndDelete(id)
+
+        if (product) {
+            return res.status(200).send(product)
+        }
+
+        res.status(404).send({ error: "Producto no encontrado" })
+    } catch (error) {
+        res.status(500).send({ error: `Error en eliminar producto ${error}` })
+    }
+}
