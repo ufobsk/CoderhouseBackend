7f440691632ba5eab58c093509919e73fb2b6487
fnkbsk <igoico10@gmail.com>
2023-10-31 13:34:04 -0300

    commit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,2 +1,23 @@
 import { Router } from 'express';
-import routerCart from './carts.routes';
+import routerCart from './carts.routes.js';
+import routerMessage from './messages.routes.js';
+import routerProd from './prod.routes.js';
+import routerUser from './users.routes.js';
+import routerSession from './sessions.routes.js';
+import routerHandlebars from './handlebars.routes.js';
+import routerMailing from './mail.routes.js';
+import routerTicket from './tickets.routes.js';
+
+const router = Router();
+
+router.use('/api/products', routerProd);
+router.use('/api/messages', routerMessage);
+router.use('/api/carts', routerCart);
+router.use('/api/users', routerUser);
+router.use('/api/sessions', routerSession);
+router.use('/api/mail', routerMailing);
+router.use('/api/tickets', routerTicket);
+
+router.use('/static', routerHandlebars);
+
+export default router;
