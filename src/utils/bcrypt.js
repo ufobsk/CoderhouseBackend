62d8c80fb525e80ad15effe3faa633bbe658a64f
fnkbsk <igoico10@gmail.com>
2023-10-31 13:13:23 -0300

    commit

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
 
-//Encriptar contraseña
-export const createHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)));
+// encriptar contraseña
+export const createHash = password =>
+	bcrypt.hashSync(password, bcrypt.genSaltSync(parseInt(process.env.SALT)));
 
-//Validar contraseña encriptada
-export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD);
+export const validatePassword = (passwordSended, passwordBBDD) =>
+	bcrypt.compareSync(passwordSended, passwordBBDD);
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,28 +1,33 @@
-import "dotenv/config";
-import jwt from "jsonwebtoken";
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 
-export const generateToken = (user) => {
-  const token = jwt.sign({ user }, "coderhouse123", { expiresIn: "12h" });
-  return token;
+export const generateToken = user => {
+	/* 
+    1° parámetro: Objeto asociado del token
+    2° parámetro: Clave privada para el cifrado de los datos
+    3° patámetro: Tiempo de vida
+  */
+	const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '12h' });
+	return token;
 };
 
-
-console.log(generateToken());
-
 export const authToken = (req, res, next) => {
-  const authHeader = req.headers.Authorization;
-  if (!authHeader) {
-    return res.status(401).send({ error: "Usuario no autenticado" });
-  }
+	// Consulto el header
 
-  const token = authHeader.split(" ")[1];
+	const authHeader = req.cookies.jwtCookie; // Consulto si existe el token
 
-  jwt.sign(token, process.env.JWT_SECRET, (error, credentials) => {
-    if (error) {
-      return res.status(403).send({ error: "Usuario no autorizado" });
-    }
+	if (!authHeader) {
+		return res.status(401).send({ error: 'Usuario no autenticado' });
+	}
 
-    req.user = credentials.user;
-    next();
-  });
+	const token = authHeader.split(' ')[1]; // Separo en dos el token y me quedo con el token en sí
+	jwt.sign(token, process.env.JWT_SECRET, (error, credentials) => {
+		if (error) {
+			return res.status(403).send({ error: 'Usuario no autorizado' });
+		}
+		//descifro el token
+		req.user = credentials.user;
+
+		next();
+	});
 };
