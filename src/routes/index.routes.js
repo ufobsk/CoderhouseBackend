ea82e3a0df726f5be4e5744df4f254e95dc998ed
fnkbsk <igoico10@gmail.com>
2023-11-28 12:14:58 -0300

    commit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import routerCart from './carts.routes.js';
 import routerMessage from './messages.routes.js';
-import routerProd from './prod.routes.js';
+import routerProd from './products.routes.js';
 import routerUser from './users.routes.js';
 import routerSession from './sessions.routes.js';
 import routerHandlebars from './handlebars.routes.js';
