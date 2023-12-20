81452041d71128168ad93a07b27d1681d37be449
fnkbsk <igoico10@gmail.com>
2023-12-19 21:21:15 -0300

    commit

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,152 +1,71 @@
-import 'dotenv/config';
-import local from "passport-local";
-import passport from "passport";
-import jwt, { ExtractJwt } from "passport-jwt";
-import GithubStrategy from "passport-github2";
-import { createHash, validatePassword } from "../utils/bcrypt.js";
-import userModel from "../models/users.models.js";
-import cartModel from '../models/carts.models.js';
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { Strategy as GitHubStrategy } from 'passport-github';
+import bcrypt from 'bcrypt';
+import userModel from '../models/user.models.js';
 
-const LocalStrategy = local.Strategy;
-const JWTStrategy = jwt.Strategy;
-
-
-const initializePassport = () => {
-  jwtLogin();
-  localRegister();
-  localLogin();
-  githubRegister();
-  initializeSession();
-  removeSession();
-};
-
-const localRegister = () => {
-  passport.use(
-    'register',
-    new LocalStrategy(
-      {
-        passReqToCallback: true,
+const setupPassport = () => {
+    passport.use(new LocalStrategy({
         usernameField: 'email',
-      },
-      async (req, username, password, done) => {
-        const { first_name, last_name, email, age } = req.body;
+        passwordField: 'password'
+    },
+        async (email, password, done) => {
+            try {
+                const user = await userModel.findOne({ email });
+                if (!user || !(await bcrypt.compare(password, user.password))) {
+                    return done(null, false, { message: 'Usuario o contraseña incorrectos' });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err);
+            }
+        }
+    ));
 
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL
+    },
+        async function (accessToken, refreshToken, profile, done) {
+            const { id, username, emails } = profile;
+            try {
+                const user = await userModel.findOne({ githubId: id });
+
+                if (!user) {
+                    const newUser = new userModel({
+                        githubId: id,
+                        first_name: username || 'Nombre por defecto',
+                        email: emails ? emails[0].value : 'default@email.com',
+                        age: 18,
+                        password: Math.random().toString(36).substring(7),
+                        last_name: '',
+                        role: 'user',
+                    });
+
+                    await newUser.save();
+                    return done(null, newUser);
+                }
+
+                return done(null, user);
+            } catch (err) {
+                return done(err);
+            }
+        }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(async (id, done) => {
         try {
-					const user = await userModel.findOne({ email: username });
-					if (user) {
-						return done('Usuario existente');
-					}
-					const passwordHash = createHash(password);
-					const userCreated = await userModel.create({
-						first_name: first_name,
-						last_name: last_name,
-						email: email,
-						age: age,
-						password: passwordHash,
-					});
-					req.user = userCreated;
-					return done(null, userCreated);
-				} catch (error) {
-					return done(error);
-				}
-      }
-    )
-  );
-};
+            const user = await userModel.findById(id).select('-password');
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
+    });
+}
 
-const localLogin = () => {
-  passport.use(
-		'login',
-		new LocalStrategy(
-			{
-				usernameField: 'email',
-			},
-			async (username, password, done) => {
-				try {
-					const user = await userModel.findOne({ email: username });
-					if (!user) {
-						return done(null, false);
-					}
-					if (validatePassword(password, user.password)) {
-						return done(null, user);
-					}
-
-					return done(null, false);
-				} catch (error) {
-					return done(error);
-				}
-			}
-		)
-	);
-};
-
-const githubRegister = () => {
-	passport.use(
-		'github',
-		new GithubStrategy(
-			{
-				clientID: process.env.CLIENT_ID,
-				clientSecret: process.env.CLIENT_SECRET,
-				callbackURL: process.env.CALLBACK_URL,
-			},
-			async (accessToken, refreshToken, profile, done) => {
-				try {
-					const user = await userModel.findOne({ email: profile._json.email });
-					if (user) {
-						done(null, user);
-					} else {
-						const userCreated = await userModel.create({
-							first_name: profile._json.name,
-							last_name: ' ',
-							email: profile._json.email,
-							age: 18,
-							password: 'password',
-						});
-						done(null, userCreated);
-					}
-				} catch (error) {
-					done(error);
-				}
-			}
-		)
-	);
-};
-
-const jwtLogin = () => {
-	const cookieExtractor = req => {
-		const token = req.cookies ? req.cookies.jwtCookie : {};
-		return token;
-	};
-
-	passport.use(
-		'jwt',
-		new JWTStrategy(
-			{
-				jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]), // consulto el token de las cookies
-				secretOrKey: process.env.JWT_SECRET,
-			},
-			async (jwt_payload, done) => {
-				try {
-					return done(null, jwt_payload);
-				} catch (error) {
-					return done(error);
-				}
-			}
-		)
-	);
-};
-
-const removeSession = () => {
-	passport.deserializeUser(async (id, done) => {
-		const user = await userModel.findById(id);
-		done(null, user);
-	});
-};
-
-const initializeSession = () => {
-	passport.serializeUser((user, done) => {
-		done(null, user._id);
-	});
-};
-
-export default initializePassport;
+export default setupPassport;
