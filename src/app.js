541944471fe43595868e49a6af4c5d366bacb784
fnkbsk <igoico10@gmail.com>
2024-01-05 17:00:34 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,121 +1,118 @@
-//IMPORTACIONES
-import 'dotenv/config';
-
-
-import express from "express";
-import session from 'express-session';
-import { engine } from "express-handlebars";
-import { Server } from 'socket.io';
-import { __dirname } from "./path.js";
-import path from "path";
-import cookieParser from 'cookie-parser';
+import 'dotenv/config'
 import mongoose from 'mongoose';
-import MongoStore from 'connect-mongo';
+import express from 'express';
+import session from 'express-session';
+import MongoDBStore from 'connect-mongodb-session';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import passport from 'passport';
-import initializePassport from './config/passport.js';
+import setupPassport from './config/passport.js';
+import setupSockets from './socket.js';
+import routes from './routes/index.js'
+import errorHandler from './middlewares/errors/errorHandler.js'
+import Logger from './services/logger.js';
+import swaggerUiExpress from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
-import messageModel from "./models/messages.models.js";
-import productModel from "./models/products.models.js";
-import routerHandlebars from './routes/handlebars.routes.js';
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const port = 3000;
 
-const PORT = 8080;
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Documentacion eCommerce Backend",
+            version: "1.0.0",
+            description: "API eCommerce Backend",
+        },
+    },
+    apis: [`${__dirname}/docs/**/*.yaml`],
+};
 
+const specs = swaggerJSDoc(options);
+app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
-
-// Server
-const server = app.listen(PORT, () => {
-	console.log(`Servidor desde puerto: ${PORT}`);
-	console.log(`http://localhost:${PORT}`);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-const io = new Server(server);
+const db = mongoose.connection;
 
-//Middlewares
-function auth(req, res, next) {
-	if (req.session.emial === 'admin@admin.com') {
-		return next();
-	} else {
-		res.send('No tenés acceso a este contenido');
-	}
-}
+db.on('error', (error) => {
+    Logger.error('Error de conexión a MongoDB: ' + error);
+});
+
+db.once('open', () => {
+    Logger.info('Conexión exitosa a MongoDB.');
+});
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.SIGNED_COOKIE)); // firmo la cookie para que si se modifica la cookie no la acepte / lea
-app.use(
-	session({
-		// configuración de la sesión de mi aplicación
-		store: MongoStore.create({
-			mongoUrl: process.env.MONGO_URL,
-			mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-			ttl: 90, // Time to live - Cuanto va a durar la sesión - Segundos, no milisengundos
-		}),
-		secret: process.env.SESSION_SECRET,
-		resave: true,
-		saveUninitialized: true,
-	})
-);
-app.engine('handlebars', engine()); //defino que mi motor de plantillas va a ser handlebars
-app.set('view engine', 'handlebars');
-app.set('views', path.resolve(__dirname, './views'));
 
-//passport
-initializePassport();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const MongoDBSessionStore = MongoDBStore(session);
+
+const store = new MongoDBSessionStore({
+    uri: process.env.MONGO_URL,
+    collection: "session",
+});
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+        secure: false,
+        maxAge: 172800000
+    }
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// conexión con base de datos
+setupPassport();
 
-/* no va a ser necesario porque ya se conecta antes cuando se crea la sesión*/
-
-mongoose
-	.connect(process.env.MONGO_URL)
-	.then(() => console.log('DB conectada'))
-	.catch(error => console.log(`Error en conexión a MongoDB Atlas:  ${error}`));
-
-// Conexión con socket.io
-
-io.on('connection', socket => {
-	console.log('Conexión con Socket.io');
-
-	socket.on('load', async () => {
-		const data = await productModel.paginate({}, { limit: 5 });
-		socket.emit('products', data);
-	});
-
-	socket.on('loadCart', async () => {
-		const cart = await cartModel.findById(cartId).populate('products.id_prod');
-		if (cart) {
-			socket.emit('cartProducts', { products: cart.products, cid: cartId });
-		} else {
-			socket.emit('cartProducts', false);
-		}
-	});
-
-	socket.on('newProduct', async product => {
-		await productModel.create(product);
-		const products = await productModel.find();
-
-		socket.emit('products', products);
-	});
-
-	socket.on('mensaje', async info => {
-		const { email, message } = info;
-		await messageModel.create({
-			email,
-			message,
-		});
-		const messages = await messageModel.find();
-
-		socket.emit('mensajes', messages);
-	});
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
 });
 
-// Routes
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use('/static', routerHandlebars);
+app.use('/', routes);
+app.use(errorHandler);
 
-app.use('/', router);
+app.get('/api/current_user', (req, res) => {
+    if (req.user) {
+        return res.send(req.user);
+    } else {
+        return res.status(401).send({ error: 'Usuario no autenticado.' });
+    }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    path: '/socket.io'
+});
+
+setupSockets(io);
+
+server.listen(port, () => {
+    Logger.info(`Servidor escuchando en el puerto ${port}`);
+});
+
+export { app, io };
